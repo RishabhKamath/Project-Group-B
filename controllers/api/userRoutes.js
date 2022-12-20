b9953597a7eb1ed0e68e075ad0c211ffad2604e0
rishabh.kamath <rshbhk22@gmail.com>
2022-12-19 20:44:38 -0600

    heroku

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Recipe } = require('../../models/Index');
+const { User, Recipe } = require('../../models/');
 const withAuth = require('../../utils/auth')
 //    /api/users/    //
 
